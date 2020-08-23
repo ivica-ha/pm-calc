d6acc4df470705ac066c1cb2a03c7f4dcaf2460e
ivica-ha <ivicahalambek@gmail.com>
2020-08-23 11:30:01 +0200

    Updates

---
+++ precache-manifest.60d7216a297e34429cc1ca8be08c2a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2661041a28705084282c4005e3534ee",
+    "url": "/pm-calc/index.html"
+  },
+  {
+    "revision": "a9f74bbe5753a83522bf",
+    "url": "/pm-calc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7fe7c9e4c0b38f6d56e",
+    "url": "/pm-calc/static/js/2.d572290e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pm-calc/static/js/2.d572290e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f74bbe5753a83522bf",
+    "url": "/pm-calc/static/js/main.457bdb3d.chunk.js"
+  },
+  {
+    "revision": "8212f930ba16c308475d",
+    "url": "/pm-calc/static/js/runtime-main.17611498.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pm-calc/static/media/logo.5d5d9eef.svg"
+  }
+]);
